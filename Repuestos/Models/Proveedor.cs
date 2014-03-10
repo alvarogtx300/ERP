8198bf712743573f4f32849405d29696de5594be
alvarogtx300 <alvarogtx300@gmail.com>
2014-03-10 18:22:13 +0100

    Creados modelos repuestos

--- Repuestos/Models/Proveedor.cs
+++ Repuestos/Models/Proveedor.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repuestos.Models {
-    class Proveedor {
+    class Proveedor : ObservableObject{
+        private string cif;
+        private string nombre;
+        private string descripcion;
+
+        public string Cif { 
+            get{ return cif; }
+            set{ SetProperty(ref cif, value, () => Cif); }
+        }
+
+        public string Nombre {
+            get { return nombre; }
+            set { SetProperty(ref nombre, value, () => Nombre); }
+        }
+
+        public string Descripccion {
+            get { return descripcion; }
+            set { SetProperty(ref descripcion, value, () => Descripccion; }
+        }
     }
 }
--- Repuestos/Models/Repuesto.cs
+++ Repuestos/Models/Repuesto.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repuestos.Models {
-    class Repuesto {
+    class Repuesto : ObservableObject{
+        private int codigo;
+        private string nombre;
+        private string direccion;
+        private double precio;
+        private int numArticulos;
+
+        public int Codigo {
+            get { return codigo; }
+            set { SetProperty(ref codigo, value, () => Codigo); }
+        }
+
+        public string Nombre {
+            get { return nombre; }
+            set { SetProperty(ref nombre, value, () => Nombre); }
+        }
+
+        public string Direccion {
+            get { return direccion; }
+            set { SetProperty(ref direccion, value, () => Direccion); }
+        }
+
+        public double Precio {
+            get { return precio; }
+            set { SetProperty(ref precio, value, () => Precio); }
+        }
+
+        public int NumArticulos {
+            get { return numArticulos; }
+            set { SetProperty(ref numArticulos, value, () => NumArticulos); }
+        }
     }
 }
