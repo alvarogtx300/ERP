ffd4e8ae31f9346b89d588870b9c4796ff87aa2f
Sefiroz <cristian.gonzalez.morante@gmail.com>
2014-03-10 17:56:10 +0100

    Corregido error de namespace

--- Clientes/Views/Principal.xaml.cs
+++ Clientes/Views/Principal.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Clientes {
+namespace Clientes.Views {
     public partial class Principal : Window {
         public Principal() {
             InitializeComponent();
