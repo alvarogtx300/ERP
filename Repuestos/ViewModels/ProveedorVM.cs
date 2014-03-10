2216dfdc31969ce9c5e3f91568d28df7bae3cf37
alvarogtx300 <alvarogtx300@gmail.com>
2014-03-10 21:26:52 +0100

    Añadidas propieades VM repuestos

--- Repuestos/ViewModels/ProveedorVM.cs
+++ Repuestos/ViewModels/ProveedorVM.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Framework;
+using Repuestos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repuestos.ViewModels {
-    class ProveedorVM {
-    }
+	class ProveedorVM : ViewModelBase<Proveedor>{
+
+		public string Cif {
+			get { return Model.Cif; }
+			set { Model.Cif = value; }
+		}
+
+		public string Nombre {
+			get { return Model.Nombre; }
+			set { Model.Nombre = value; }
+		}
+
+		public string Descripccion {
+			get { return Model.Descripccion; }
+			set { Model.Descripccion = value; }
+		}
+	}
 }
--- Repuestos/ViewModels/RepuestoVM.cs
+++ Repuestos/ViewModels/RepuestoVM.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using Framework;
+using Repuestos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repuestos.ViewModels {
-    class RepuestoVM {
-    }
+	class RepuestoVM : ViewModelBase<Repuesto>{
+
+		public int Codigo {
+			get { return Model.Codigo; }
+			set { Model.Codigo = value; }
+		}
+
+		public string Nombre {
+			get { return Model.Nombre; }
+			set { Model.Nombre = value; }
+		}
+
+		public string Direccion {
+			get { return Model.Direccion; }
+			set { Model.Direccion = value; }
+		}
+
+		public double Precio {
+			get { return Model.Precio; }
+			set { Model.Precio = value; }
+		}
+
+		public int NumArticulos {
+			get { return Model.NumArticulos; }
+			set { Model.NumArticulos = value; }
+		}
+	}
 }
