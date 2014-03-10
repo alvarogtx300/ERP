a670da1bb414f27dfc4bfb76e157b6575ef3d7e0
Sefiroz <cristian.gonzalez.morante@gmail.com>
2014-03-10 20:37:33 +0100

    añado propiedades de VM

--- Clientes/ViewModels/ClienteVM.cs
+++ Clientes/ViewModels/ClienteVM.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Clientes.Models;
+using Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Clientes.ViewModels {
-    class ClienteVM {
+    class ClienteVM : ViewModelBase<Cliente> {
+        public string Nombre {
+            get { return Model.Nombre; }
+            set { Model.Nombre=value; }
+        }
+
+        public string Apellidos {
+            get { return Model.Apellidos; }
+            set { Model.Apellidos=value; }
+        }
+
+        public string Dni {
+            get { return Model.Dni; }
+            set { Model.Dni=value; }
+        }
+
+        public string Direccion {
+            get { return Model.Direccion; }
+            set { Model.Direccion=value; }
+        }
+
+        public long Telefono {
+            get { return Model.Telefono; }
+            set { Model.Telefono=value; }
+        }
     }
 }
--- Clientes/ViewModels/VehiculoVM.cs
+++ Clientes/ViewModels/VehiculoVM.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Clientes.Models;
+using Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Clientes.ViewModels {
-    class VehiculoVM {
+    class VehiculoVM : ViewModelBase<Vehiculo> {
+        public string Modelo {
+            get { return Model.Modelo; }
+            set { Model.Modelo=value; }
+        }
+
+        public string Matricula {
+            get { return Model.Matricula; }
+            set { Model.Matricula=value; }
+        }  
     }
 }
