a18d9de032449ed225bc345682214f4ebd936ebd
alvarogtx300 <alvarogtx300@gmail.com>
2014-03-13 21:59:18 +0100

    Haciendo detalle de vehiculo

--- Clientes/ViewModels/VehiculoVM.cs
+++ Clientes/ViewModels/VehiculoVM.cs
@@ -10,12 +10,38 @@ namespace Clientes.ViewModels {
     public class VehiculoVM : ViewModelBase<Vehiculo> {
         public string Modelo {
             get { return Model.Modelo; }
-            set { Model.Modelo=value; }
+            set {
+				if (string.IsNullOrWhiteSpace(value)) {
+					Model.Modelo = value;
+					OnPropertyChanged("IsOk");
+					throw new ArgumentException("Debe escribir un modelo.");
+				}
+				else {
+					Model.Modelo = value;
+					OnPropertyChanged("IsOk");
+				}
+			}
         }
 
         public string Matricula {
             get { return Model.Matricula; }
-            set { Model.Matricula=value; }
+            set {
+				if (string.IsNullOrWhiteSpace(value)) {
+					Model.Matricula = value;
+					OnPropertyChanged("IsOk");
+					throw new ArgumentException("Debe escribir una matricula.");
+				}
+				else {
+					Model.Matricula = value;
+					OnPropertyChanged("IsOk");
+				}
+			}
         }
+
+		public bool IsOk {
+			get {
+				return !string.IsNullOrWhiteSpace(Modelo) && !string.IsNullOrWhiteSpace(Matricula);
+			}
+		}
     }
 }
