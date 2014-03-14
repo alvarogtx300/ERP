724649720a59ad75463a4a3926d57ca52f60042d
alvarogtx300 <alvarogtx300@gmail.com>
2014-03-14 18:29:21 +0100

    Agregado IDAO e implementacion

---
+++ Ventas/Logic/DatosVentas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Models;
+
+namespace Ventas.Logic {
+	public class DatosVentas : IDAOVentas{
+		private static readonly Lazy<DatosVentas> instance = new Lazy<DatosVentas>(() => new DatosVentas());
+		private ObservableCollection<Venta> ventas;
+
+		private DatosVentas() {
+			ventas = new ObservableCollection<Venta>();
+		}
+
+		public static DatosVentas Instance {
+			get { return instance.Value; }
+		}
+
+		public void AgregarVenta(Venta v) {
+			ventas.Add(v);
+		}
+
+		public ObservableCollection<Venta> ListarVentas() {
+			return ventas;
+		}
+	}
+}
---
+++ Ventas/Logic/IDAOVentas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Models;
+
+namespace Ventas.Logic {
+	interface IDAOVentas {
+		ObservableCollection<Venta> ListarVentas();
+		void AgregarVenta(Venta v);
+	}
+}
