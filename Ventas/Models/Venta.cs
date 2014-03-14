177a75806e5512729cfa0710d121b5328f62bff2
alvarogtx300 <alvarogtx300@gmail.com>
2014-03-14 18:12:17 +0100

    Agregado modelo Venta

---
+++ Ventas/Models/Venta.cs
@@ -0,0 +1,32 @@
+﻿using Clientes.Models;
+using Framework;
+using Repuestos.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas.Models {
+	class Venta : ObservableObject{
+		private Cliente cliente;
+		private ObservableCollection<Repuesto> repuestos;
+		private DateTime fecha;
+
+		public Cliente Cliente {
+			get { return cliente; }
+			set { SetProperty(ref cliente, value, () => Cliente); }
+		}
+
+		public ObservableCollection<Repuesto> Repuestos {
+			get { return repuestos; }
+			set { SetProperty(ref repuestos, value, () => Repuestos); }
+		}
+
+		public DateTime Fecha {
+			get { return fecha; }
+			set { SetProperty(ref fecha, value, () => Fecha); }
+		}
+	}
+}
